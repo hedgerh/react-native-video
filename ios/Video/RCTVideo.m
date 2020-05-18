5bfdea5c5cdf775ac8485c19a229db11e3837027
Harry Hedger <hedgerh@gmail.com>
2020-05-18 16:58:47 -0700

    removed the thing

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -697,19 +697,6 @@ static int const RCTVideoUnset = -1;
   } else if (object == _playerViewController.contentOverlayView) {
       // when controls==true, this is a hack to reset the rootview when rotation happens in fullscreen
       if ([keyPath isEqualToString:@"frame"]) {
-
-        CGRect oldRect = [change[NSKeyValueChangeOldKey] CGRectValue];
-        CGRect newRect = [change[NSKeyValueChangeNewKey] CGRectValue];
-
-        if (!CGRectEqualToRect(oldRect, newRect)) {
-          if (CGRectEqualToRect(newRect, [UIScreen mainScreen].bounds)) {
-            NSLog(@"in fullscreen");
-          } else NSLog(@"not fullscreen");
-
-          [self.reactViewController.view setFrame:[UIScreen mainScreen].bounds];
-          [self.reactViewController.view setNeedsLayout];
-        }
-
         return;
       }
   } else if ([super respondsToSelector:@selector(observeValueForKeyPath:ofObject:change:context:)]) {
